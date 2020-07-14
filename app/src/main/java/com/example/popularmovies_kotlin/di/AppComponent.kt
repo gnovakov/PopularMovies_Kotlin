cf9f2eecdd69043dea42a06802173fbad668b304
Goran Novakovic <gnovakov@gmail.com>
2020-07-14 22:42:13 +0100

    extra cleanup

--- app/src/main/java/com/example/popularmovies_kotlin/di/AppComponent.kt
+++ app/src/main/java/com/example/popularmovies_kotlin/di/AppComponent.kt
@@ -12,25 +12,6 @@ import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
-//@Singleton
-//@Component(
-//    modules = [
-//        AppModule::class,
-//        ApiModule::class,
-//        ActivityModule::class
-//    ]
-//)
-//
-//interface AppComponent : AndroidInjector<App> {
-//    @Component.Builder
-//    interface Builder {
-//        @BindsInstance
-//        fun application(application: Application): Builder
-//
-//        fun build(): AppComponent
-//    }
-//}
-
 @Singleton
 @Component(modules = [AppModule::class, ApiModule::class])
 interface AppComponent {
